2c22ad96508675fbc8c2b59fb8209ec5fcd3d1fc
A01749746 <a01749746@tec.mx>
2024-02-28 13:09:21 -0600

    Pong Final

--- Unity/Assets/Scripts/GameController.cs
+++ Unity/Assets/Scripts/GameController.cs
@@ -8,26 +8,83 @@ Alberto Limón
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject dotPrefab;
     public GameObject dot;
     public float force;
+
+    public int pointsLeft = 0;
+    public int pointsRight = 0;
+
+    public TMP_Text leftScore;
+    public TMP_Text rightScore;
+
+    public int maxPoints;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartGame();
+        StartGame();       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            Reset();
+        }
     }
 
-    void StartGame ()
+    public void StartGame()
     {
+        pointsLeft =0;
+        pointsRight =0;
+        leftScore.text = pointsLeft.ToString();
+        rightScore.text = pointsRight.ToString();
+        ServeBall();
+
+    }
+
+    public void ServeBall()
+    {
+        Destroy(dot);
+        // Create a copy of the prefab object
+        dot = Instantiate(dotPrefab);
         dot.GetComponent<Rigidbody2D>().velocity = Random.onUnitSphere * force;
+
+    }
+
+    public void Reset()
+    {
+        Destroy(dot);
+        ServeBall();
+    }
+
+    public void AddPoints(int side)
+    {
+        if (side == 1)
+        {
+            pointsLeft++;
+            if(pointsLeft >= maxPoints){
+                leftScore.text = "Winner! " + pointsLeft.ToString();
+            }else{
+                leftScore.text = pointsLeft.ToString();
+                Reset();
+            }
+        }
+        else
+        {
+            pointsRight++;
+             if(pointsRight >= maxPoints){
+                rightScore.text = "Winner! " + pointsRight.ToString();
+            }else{
+                rightScore.text = pointsRight.ToString();
+                Reset();
+            }
+        }
     }
 }
